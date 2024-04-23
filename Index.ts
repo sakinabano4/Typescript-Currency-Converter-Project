343d64c9b52cb50c676bce355c752524cc9725cd
sakinabano4 <sakinabano4575@gmail.com>
2024-04-23 17:19:45 +0500

    Adding Currency Converter Project

---
+++ Index.ts
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+console.log(chalk.yellow.bold("\n\t Wellcome to 'Sakina Bano'-Currency Convertor\n"));
+
+//Define the list of currencies and thier exchange rates
+let exchange_rate: any = {
+    "USD":  1 ,   //Base Currency
+    "EURO": 0.9,  // European Currency(Euro)
+    "JYP" : 113,  // japenese Currency(Yen)
+    "CAD":  1.3,  // Canadian Dollar
+    "AUD":  1.65, //Australin Dollar
+    "PKR":  277.7,  // Pakistan Rupees
+    // Add more currencies and thier exchange rates here
+}
+
+// Print the user to select currencies to convert from and to
+let user_answer = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message:"sellect the currency to convert from",
+        choices:["USD","EUR","JYP","CAD","AUD","PKR"]
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: "sellect the currency to convert into",
+        choices:["USD","EUR","JYP","CAD","AUD","PKR"]
+   
+    },
+    {
+        name:"amount",
+        type: "input",
+        message:"Enter the amount to convert:"
+    }
+]);
+
+// perfoorm currency conversion by using Formula
+let from_amount = exchange_rate[user_answer.from_currency];
+let to_amount = exchange_rate[user_answer.to_currency];
+let amount = user_answer.amount
+ 
+//Formula of conversion
+let base_amount = amount / from_amount
+let converted_amount = base_amount * to_amount
+
+//Display the converted amount
+console.log(`converted Amount = ${chalk.green(converted_amount.toFixed(2))}`);
